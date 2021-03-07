ead0233c34bf09920405e3988c545ccc4c0185b9
unknown <wujiahong19981022@outlook.com>
2021-03-07 19:32:41 +0800

    [init] 初始化代码结构

---
+++ polar/bin/polar_test.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+"""
+======================================================
+Author: wujiahong
+Filename: test.py
+Description: 测试数学库
+======================================================
+"""
+from polar.libcommon import test
+
+if __name__ == "__main__":
+    pass
---
+++ polar/test/test.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+======================================================
+Author: wujiahong
+Filename: test.py
+Description: 测试数学库
+======================================================
+"""
+
+import math
+import numpy as np
+
+if __name__ == "__main__":
+    a = np.array([[1,0],[1,1]])
+    r = np.kron(a,a)
+    print(r)
+    pass
